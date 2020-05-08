98890c7d292dd4ecb5312201b5822ff5361b2a61
eonsubae <eonsubae@naver.com>
2020-05-08 20:19:59 +0900

    Update folder structure

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
