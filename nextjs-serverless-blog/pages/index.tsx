444b76910a657f23ea16e71cc046b99c6fa24f3f
eonsubae <eonsubae@naver.com>
2020-07-22 22:50:12 +0900

    Update index page to divide the sections

--- nextjs-serverless-blog/pages/index.tsx
+++ nextjs-serverless-blog/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet';
 
+import '../styles/components/index.scss';
+
 export default function Home() {
   return (
     <>
@@ -7,8 +9,23 @@ export default function Home() {
         <title>Eonsu Bae</title>
         <meta name="description" content="A Cutting Edge Engineer from Korea" />
       </Helmet>
-      <h1>Eonsu Bae</h1>
-      <h3>Cutting Edge Engineer</h3>
+      <main className="index-main">
+        <section className="introduce-section">
+          <div>
+            <p>Hello,</p>
+          </div>
+          <div>
+            <h1>I'm Eonsu Bae</h1>
+          </div>
+          <div>
+            <h2>A Cutting Edge Engineer from Korea</h2>
+          </div>
+          <div>
+            <p>Contact Me</p>
+          </div>
+        </section>
+        <section className="photo-section"></section>
+      </main>
     </>
   );
 }
