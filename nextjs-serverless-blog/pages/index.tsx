57eaee7dadef1967645d2632fe93cbf70bdf87a8
eonsubae <eonsubae@naver.com>
2020-07-22 22:34:31 +0900

    Add react-helmet for the SEO

--- nextjs-serverless-blog/pages/index.tsx
+++ nextjs-serverless-blog/pages/index.tsx
@@ -1,6 +1,12 @@
+import { Helmet } from 'react-helmet';
+
 export default function Home() {
   return (
     <>
+      <Helmet>
+        <title>Eonsu Bae</title>
+        <meta name="description" content="A Cutting Edge Engineer from Korea" />
+      </Helmet>
       <h1>Eonsu Bae</h1>
       <h3>Cutting Edge Engineer</h3>
     </>
